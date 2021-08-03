03fe701d657f736aff61621bdd0b8f768c8b3442
steph-hyj <ungstephane0@gmail.com>
2021-08-03 17:37:21 +0200

    Project ZUP of Tranzition Account

---
+++ client/main.js
@@ -0,0 +1,281 @@
+// YOUR JAVASCRIPT CODE FOR INDEX.HTML GOES HERE
+function createLead() {
+    debugger;
+    $("#loader").show();
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    if (lastName == "") {
+        alert("Kindly Enter the Last Name");
+        $("#loader").hide();
+        return;
+    }
+    var companyName = $("#companyName").val();
+    var email = $("#email").val();
+    var state = $("#state").val();
+    var phone = $("#phone").val();
+    var leadSource = $("#leadSource").val();
+
+    $.ajax({
+        url: "/server/crm_crud/crmData",
+        type: "post",
+        contentType: "application/json",
+        data: JSON.stringify({
+            "First_Name": firstName,
+            "Last_Name": lastName,
+            "Company": companyName,
+            "Email": email,
+            "State": state,
+            "Phone": phone,
+            "Lead_Source": leadSource,
+        }),
+        success: function (data) {
+            debugger;
+            $('#leads').trigger("reset");
+            $("#myModalLabel").html("Success");
+            $("#message").html("Lead Created Successfully");
+            $("#loader").hide();
+            $('#myModal').modal('show');
+        },
+        error: function (error) {
+            $('#leads').trigger("reset");
+            $("#myModalLabel").html("Failure");
+            $("#message").html("Please try again after Sometime");
+            $("#loader").hide();
+            $('#myModal').modal('show');
+        }
+    });
+}
+
+function logout() {
+    var redirectURL = "http://localhost:3000/app/signin.html"; //Add your app domain
+    var auth = catalyst.auth;
+    auth.signOut(redirectURL);
+}
+
+function getUserDetails() {
+    $("#main").hide();
+    $("#connect").hide();
+    catalyst.auth.isUserAuthenticated().then(result => {
+        $("#loader").show();
+        $.ajax({
+            url: "/server/crm_crud/getUserDetails",
+            type: "get",
+            success: function (data) {
+                $("#loader").hide();
+                if (data.userId) {
+                    $("#main").show();
+                    $("#connect").hide();
+                } else {
+                    $("#connect").show();
+                    $("#main").hide();
+                }
+            },
+            error: function (error) {
+                $("#myModalLabel").html("Failure");
+                $("#message").html("Please try again after Sometime");
+                $("#loader").hide();
+                $('#myModal').modal('show');
+            }
+        });
+    }).catch(err => {
+        document.body.innerHTML = 'You are not logged in. Please log in to continue. Redirecting you to the login page..';
+        setTimeout(function () {
+            window.location.href = "login.html";
+        }, 3000);
+    });
+}
+
+
+
+function getLeads() {
+    debugger;
+    var tableContainer = document.getElementById("showData");
+    tableContainer.innerHTML = "";
+    $("#loaders").show();
+    $.ajax({
+        url: "/server/crm_crud/crmData",
+        type: "get",
+        success: function (data) {
+            debugger;
+            var reqData = getRequiredData(data.data);
+            $("#loaders").hide();
+            renderTable(reqData);
+        },
+        error: function (error) {
+            $("#myModalLabel").html("Failure");
+            $("#message").html("Please try again after Sometime");
+            $("#loader").hide();
+            $('#myModal').modal('show');
+        }
+    });
+
+
+}
+
+function showDeletePopup(leadID) {
+    $('#ModalDanger').modal('show');
+    var deleteBtn = document.getElementById("delete-btn");
+    deleteBtn.value = leadID;
+}
+
+function deleteLead() {
+    var leadID = document.getElementById('delete-btn').value;
+    $.ajax({
+        url: "/server/crm_crud/crmData/" + leadID,
+        type: "delete",
+        success: function (data) {
+            debugger;
+            $('#ModalDanger').modal('toggle');
+            $("#myModalLabel").html("Success");
+            $("#message").html("Lead Deleted Successfully");
+            $('#myModal').modal('show');
+            setTimeout(function () {
+                location.reload();
+            }, 3000);
+        },
+        error: function (error) {
+            $("#myModalLabel").html("Failure");
+            $("#message").html("Please try again after Sometime");
+            $("#loader").hide();
+            $('#myModal').modal('show');
+        }
+    });
+}
+
+function showEditPopup(leadID) {
+
+    $.ajax({
+        url: "/server/crm_crud/crmData/" + leadID,
+        type: "get",
+        success: function (data) {
+            debugger;
+            var respData = data.data;
+            $('#editfirstName').val(respData[0].First_Name);
+            $('#editlastName').val(respData[0].Last_Name);
+            $('#editcompanyName').val(respData[0].Company);
+            $('#editemail').val(respData[0].Email);
+            $('#editstate').val(respData[0].State);
+            $('#editphone').val(respData[0].Phone);
+            $('#editleadSource').val(respData[0].Lead_Source);
+            $('#editBtn').val(respData[0].id);
+
+            $('#editfirstName').html(respData[0].First_Name);
+            $('#editlastName').html(respData[0].Last_Name);
+            $('#editcompanyName').html(respData[0].Company);
+            $('#editemail').html(respData[0].Email);
+            $('#editstate').html(respData[0].State);
+            $('#editphone').html(respData[0].Phone);
+            $('#editleadSource').html(respData[0].Lead_Source);
+
+            $('#editForm').modal('show');
+        },
+        error: function (error) {
+            $("#myModalLabel").html("Failure");
+            $("#message").html("Please try again after Sometime");
+            $("#loader").hide();
+            $('#myModal').modal('show');
+        }
+    });
+}
+
+function editLead() {
+    var firstName = $("#editfirstName").val();
+    var lastName = $("#editlastName").val();
+    if (lastName == "") {
+        alert("Kindly Enter the Last Name");
+        return;
+    }
+    var companyName = $("#editcompanyName").val();
+    var email = $("#editemail").val();
+    var state = $("#editstate").val();
+    var phone = $("#editphone").val();
+    var leadSource = $("#editleadSource").val();
+    var leadID = $("#editBtn").val();
+
+    $.ajax({
+        url: "/server/crm_crud/crmData/" + leadID,
+        type: "put",
+        contentType: "application/json",
+        data: JSON.stringify({
+            "First_Name": firstName,
+            "Last_Name": lastName,
+            "Company": companyName,
+            "Email": email,
+            "State": state,
+            "Phone": phone,
+            "Lead_Source": leadSource
+        }),
+        success: function (data) {
+            debugger;
+            $('#editForm').modal('toggle');
+            $("#myModalLabel").html("Success");
+            $("#message").html("Lead Edited Successfully");
+            $('#myModal').modal('show');
+            setTimeout(function () {
+                location.reload();
+            }, 3000);
+        },
+        error: function (error) {
+            $('#leads').trigger("reset");
+            $("#myModalLabel").html("Failure");
+            $("#message").html("Please try again after Sometime");
+            $("#loader").hide();
+            $('#myModal').modal('show');
+        }
+    });
+
+}
+
+
+function getRequiredData(data) {
+
+    var i;
+    var resp = [];
+    for (i = 0; i < data.length; i++) {
+        var gulp = {
+            "First Name": data[i].First_Name,
+            "Last Name": data[i].Last_Name,
+            "Phone": data[i].Phone,
+            "Email": data[i].Email,
+            "Company": data[i].Company,
+			"Edit": `<center><a href="javascript:showEditPopup('&quot;' + data[i].id + '&quot;)">&#9998;︎</a></center>`,
+            "Delete": `<center><a href="javascript:showDeletePopup(&quot;' + data[i].id + '&quot;)">&#128465;︎</a></center>`
+        }
+        resp.push(gulp);
+    }
+    console.log(resp);
+    return resp;
+}
+function renderTable(respData) {
+    var col = [];
+    for (var i = 0; i < respData.length; i++) {
+        for (var key in respData[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+    var table = document.createElement("table");
+    table.id = "dataTable";
+
+    var tr = table.insertRow(-1);
+
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+    for (var i = 0; i < respData.length; i++) {
+
+        tr = table.insertRow(-1);
+
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = respData[i][col[j]];
+        }
+    }
+
+    var divContainer = document.getElementById("showData");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+}
---
+++ functions/crm_crud/index.js
@@ -0,0 +1,288 @@
+'use strict';
+const express = require('express');
+const http = require('https')
+const app = express();
+app.use(express.json());
+const catalyst = require('zcatalyst-sdk-node');
+const HOST = 'www.zohoapis.eu';
+const AUTH_HOST = 'https://accounts.zoho.eu/oauth/v2/token';
+const PORT = 443;
+const fetch = require('node-fetch');
+const CLIENTID = '1000.R7DL1I33NXIRTFBOJR62ABWC6582QP'; //Add your client ID
+const CLIENT_SECRET = 'f56bd8485f6d7f26156ded430e7732586b769ce209'; //Add your client secret
+
+//Fetches the Refresh Token by calling the getRefreshToken() function, and inserts it along with the userID in the Token table
+app.get('/generateToken', async (req, res) => {
+
+	try {
+		const catalystApp = catalyst.initialize(req);
+		const code = req.query.code;
+
+		let userManagement = catalystApp.userManagement();
+		let userDetails = await userManagement.getCurrentUser();
+		const refresh_token = await getRefreshToken(code, res);
+		const userId = userDetails.user_id;
+		const catalystTable = catalystApp.datastore().table('Token');
+		await catalystTable.insertRow({
+			refresh_token,
+			userId
+		});
+		res.status(200).redirect("http://localhost:3000/app/index.html"); //Add your app domain
+	}
+	catch (err) {
+		console.log(err);
+		res.status(500).send({ message: 'Internal Server Error. Please try again after sometime.', error: err })
+	}
+})
+
+//Fetches the user details by calling the getUserDetails() function 
+app.get('/getUserDetails', async (req, res) => {
+
+	try {
+
+		const catalystApp = catalyst.initialize(req);
+		const userDetails = await getUserDetails(catalystApp);
+
+		if (userDetails.length !== 0) {
+			res.status(200).send({ userId: userDetails[0].Token.userId })
+		} else {
+			res.status(200).send({ userId: null })
+		}
+	}
+	catch (err) {
+		console.log(err);
+		res.status(500).send({ message: 'Internal Server Error in Getting User Details. Please try again after sometime.', error: err })
+	}
+})
+
+//Executes various APIs to access, add, or modify leads in CRM
+//Fetches all leads
+app.get('/crmData', async (req, res) => {
+
+	try {
+		const catalystApp = catalyst.initialize(req);
+		const userDetails = await getUserDetails(catalystApp);
+		const accessToken = await getAccessToken(catalystApp, userDetails);
+		const options = {
+			'hostname': HOST,
+			'port': PORT,
+			'method': 'GET',
+			'path': `/crm/v2/Leads`,
+			'headers': {
+				'Authorization': `Zoho-oauthtoken ${accessToken}`
+			}
+		};
+		var data = "";
+		const request = http.request(options, function (response) {
+			response.on('data', function (chunk) {
+				data += chunk;
+			});
+
+			response.on('end', function () {
+				console.log(response.statusCode);
+				res.setHeader('content-type', 'application/json');
+				res.status(200).send(data)
+			});
+		});
+		request.end();
+	}
+	catch (err) {
+		console.log(err);
+		res.status(500).send({ message: 'Internal Server Error. Please try again after sometime.' })
+	}
+})
+
+//Fetches a particular lead
+app.get('/crmData/:id', async (req, res) => {
+
+	try {
+		const catalystApp = catalyst.initialize(req);
+		const userDetails = await getUserDetails(catalystApp);
+		const accessToken = await getAccessToken(catalystApp, userDetails);
+		const options = {
+			'hostname': HOST,
+			'port': PORT,
+			'method': 'GET',
+			'path': `/crm/v2/Leads/${req.params.id}`,
+			'headers': {
+				'Authorization': `Zoho-oauthtoken ${accessToken}`
+			}
+		};
+		var data = "";
+		const request = http.request(options, function (response) {
+			response.on('data', function (chunk) {
+				data += chunk;
+			});
+
+			response.on('end', function () {
+				res.setHeader('content-type', 'application/json');
+				res.status(200).send(data)
+			});
+		});
+		request.end();
+	}
+	catch (err) {
+		console.log(err);
+		res.status(500).send({ message: 'Internal Server Error. Please try again after sometime.' })
+	}
+})
+
+//Creates a new lead
+app.post('/crmData', async (req, res) => {
+
+	try {
+		const catalystApp = catalyst.initialize(req);
+		const createData = req.body;
+		const reqData = [];
+		reqData.push(createData)
+		const data = {
+			"data": reqData
+		}
+		if (!createData) {
+			res.status(400).send({ 'message': 'Data Not Found' });
+		}
+		const userDetails = await getUserDetails(catalystApp);
+		const accessToken = await getAccessToken(catalystApp, userDetails);
+		const options = {
+			'hostname': HOST,
+			'port': PORT,
+			'method': 'POST',
+			'path': `/crm/v2/Leads`,
+			'headers': {
+				'Authorization': `Zoho-oauthtoken ${accessToken}`,
+				'Content-Type': 'application/json'
+			}
+		};
+		const request = http.request(options, function (response) {
+			res.setHeader('content-type', 'application/json');
+			response.pipe(res);
+		});
+		request.write(JSON.stringify(data));
+		request.end();
+	}
+	catch (err) {
+		console.log(err);
+		res.status(500).send({ message: 'Internal Server Error. Please try again after sometime.' })
+	}
+
+});
+
+//Edits a particular lead
+app.put('/crmData/:id', async (req, res) => {
+
+	try {
+		const catalystApp = catalyst.initialize(req);
+		const updateData = req.body;
+		const reqData = [];
+		reqData.push(updateData)
+		const data = {
+			"data": reqData
+		}
+		if (!updateData) {
+			res.status(400).send({ 'message': 'Update Data Not Found' });
+		}
+		const userDetails = await getUserDetails(catalystApp);
+		const accessToken = await getAccessToken(catalystApp, userDetails);
+		const options = {
+			'hostname': HOST,
+			'port': PORT,
+			'method': 'PUT',
+			'path': `/crm/v2/Leads/${req.params.id}`,
+			'headers': {
+				'Authorization': `Zoho-oauthtoken ${accessToken}`,
+				'Content-Type': 'application/json'
+			}
+		};
+		const request = http.request(options, function (response) {
+
+			res.setHeader('content-type', 'application/json');
+			response.pipe(res);
+		});
+		request.write(JSON.stringify(data));
+		request.end();
+
+	}
+	catch (err) {
+		console.log(err);
+		res.status(500).send({ message: 'Internal Server Error. Please try again after sometime.' })
+	}
+
+});
+
+//Deletes a particular lead
+app.delete('/crmData/:id', async (req, res) => {
+
+	console.log(`/crm/v2/Leads/${req.params.id}`);
+	try {
+		const catalystApp = catalyst.initialize(req);
+		const userDetails = await getUserDetails(catalystApp);
+		const accessToken = await getAccessToken(catalystApp, userDetails);
+		const options = {
+			'hostname': HOST,
+			'port': PORT,
+			'method': 'DELETE',
+			'path': `/crm/v2/Leads/${req.params.id}`,
+			'headers': {
+				'Authorization': `Zoho-oauthtoken ${accessToken}`,
+				'Content-Type': 'application/json'
+			}
+		};
+		const request = http.request(options, function (response) {
+			res.setHeader('content-type', 'application/json');
+			response.pipe(res);
+		});
+		request.end();
+	}
+	catch (err) {
+		console.log(err);
+		res.status(500).send({ message: 'Internal Server Error. Please try again after sometime.' })
+	}
+
+});
+
+//Fetches an Access Token using the Refresh Token
+async function getAccessToken(catalystApp, userDetails) {
+
+	const refresh_token = userDetails[0].Token.refresh_token;
+	const userId = userDetails[0].Token.userId;
+
+	const credentials = {
+		[userId]: {
+			client_id: CLIENTID,
+			client_secret: CLIENT_SECRET,
+			auth_url: AUTH_HOST,
+			refresh_url: AUTH_HOST,
+			refresh_token
+		}
+	}
+	const accessToken = await catalystApp.connection(credentials).getConnector(userId).getAccessToken();
+	return accessToken;
+}
+
+//Fetches the Refresh Token by passing the required details
+async function getRefreshToken(code, res) {
+	try {
+		const URL = `${AUTH_HOST}?code=${code}&client_id=${CLIENTID}&client_secret=${CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=http://localhost:3000/server/crm_crud/generateToken`; //Add your app domain
+		const response = await fetch(URL, { method: 'post' })
+		const data = await response.json();
+		return data.refresh_token;
+	}
+	catch (err) {
+		console.log(err);
+		res.status(500).send({ message: 'Internal Server Error. Please try again after sometime.', error: err })
+	}
+}
+
+//Fetches the record from the Token table that contains the Refresh Token, by passing the userID
+async function getUserDetails(catalystApp) {
+
+	let userManagement = catalystApp.userManagement();
+	let userDetails = await userManagement.getCurrentUser();
+	let query = 'SELECT * FROM Token where UserId=' + userDetails.user_id;
+	let zcql = catalystApp.zcql();
+	let userDetail = await zcql.executeZCQLQuery(query);
+	return userDetail;
+
+}
+
+module.exports = app;
