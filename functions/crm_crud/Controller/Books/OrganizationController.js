a808be4fe6266dd63927ac4789aa8a3b3bde892d
steph-hyj <ungstephane0@gmail.com>
2021-08-19 10:18:23 +0200

    [Add] Back add folder to control Books

---
+++ functions/crm_crud/Controller/Books/OrganizationController.js
@@ -0,0 +1,43 @@
+'use strict';
+const catalyst = require('zcatalyst-sdk-node');
+const tokenController = require('../TokenController.js');
+const HOST = 'books.zoho.eu';
+const PORT = 443;
+const http = require('https');
+
+exports.getOrganizationID = async(req,res) => {
+    try
+    {
+        const catalystApp = catalyst.initialize(req);
+		const userDetails = await tokenController.getUserDetails(catalystApp);
+		const accessToken = await tokenController.getAccessToken(catalystApp, userDetails);
+
+        const options = {
+			'hostname': HOST,
+			'port': PORT,
+			'method': 'GET',
+			'path': '/api/v3/organizations',
+			'headers': {
+				'Authorization': `Zoho-oauthtoken ${accessToken}`
+			}
+		};
+
+		var data = "";
+		const request = http.request(options, function (response) {
+			response.on('data', function (chunk) {
+				data += chunk;
+			});
+
+			response.on('end', function () {
+				res.setHeader('content-type', 'application/json');
+				res.status(200).send(data)
+			});
+		});
+		request.end();
+    }
+    catch(err)
+    {
+		console.log(err);
+		res.status(500).send({ message: 'Internal Server Error. Please try again after sometime.' })
+    }
+};
