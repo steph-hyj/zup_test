62e3e1642da7b76309722753ce67927f60ee7d6e
steph-hyj <ungstephane0@gmail.com>
2021-08-19 10:16:55 +0200

    [Delete] Move file in other folder

--- functions/crm_crud/routes/CRM/user.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userController = require('../../Controller/userController.js');
-
-router.get('/generateToken',userController.generateToken);
-router.get('/getUserDetails',userController.getUserDetails);
-router.get('/getUserZohoID/:email',userController.getUserZohoID);
-
-module.exports = router;
