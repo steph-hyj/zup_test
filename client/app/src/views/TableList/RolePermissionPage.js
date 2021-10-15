c08d5b6aa1fd4d2c4d61fc21fd6611c261e5920c
steph-hyj <ungstephane0@gmail.com>
2021-10-15 18:08:53 +0200

    [Update] RolePermission update table to get value

--- client/app/src/views/TableList/RolePermissionPage.js
+++ client/app/src/views/TableList/RolePermissionPage.js
@@ -10,9 +10,11 @@ import CardHeader from "../../components/Card/CardHeader.js";
 import CardBody from "../../components/Card/CardBody.js";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import { Dialog, DialogActions, DialogContent, DialogTitle, MenuItem, TextField } from "@material-ui/core";
+import { Dialog, DialogActions, DialogContent, DialogTitle, MenuItem, TextField, Select, InputLabel } from "@material-ui/core";
+import FormControl from '@mui/material/FormControl';
 import AddIcon from '@mui/icons-material/Add';
 import { Box } from "@mui/system";
+import { CircularProgress } from "@mui/material";
 import axios from "axios";
 
 
@@ -59,6 +61,9 @@ export default function RolePermissionPage() {
   const classes = useStyles();
   const [roles, setRoles] = React.useState('');
   const [roleName, setRoleName] = React.useState('');
+  const [modules, setModules] = React.useState('');
+  const [moduleValue, setModuleValue] = React.useState('');
+  const [done, setDone] = React.useState(undefined);
   const [open, setOpen] = React.useState(false);
 
   useEffect(() => {
@@ -67,6 +72,39 @@ export default function RolePermissionPage() {
     }).catch((err) => {
       console.log(err)
     });
+
+    let modules = axios.get(baseUrl+"module").then((response)=>{ 
+      return response.data.modules
+    }).catch((err) => {
+      console.log(err)
+    });
+
+    const moduleTab = [];
+
+    modules.then((modules)=>{
+        modules.forEach(module => {
+            axios.get(baseUrl+"module/getFields/"+module.api_name).then((response) => {
+                var status = response.data.status;
+                if(!status) {
+                    response.data.fields.forEach((field)=>{
+                        if(field.api_name === "Email") {
+                            moduleTab.push(module)
+                        }
+                    })
+                }
+                setModules(moduleTab);
+            }).catch((err)=> {
+                console.log(err);
+            })
+        });
+    }).catch((err)=>{
+        console.log(err);
+    });
+
+    setTimeout(() => {
+      setDone(true);
+    }, 3000);
+
   },[])
 
   const handleChange = (event) => {
@@ -97,66 +135,115 @@ export default function RolePermissionPage() {
       roleName
     }).then((response)=> {
       console.log(response);
+      setOpen(false);
     }).catch((err) => {
       console.log(err);
     })
+  };
+
+  function getModule() {
+    if(modules) {
+      return(
+        modules.map(module => {
+          return <MenuItem value={module.api_name}>{module.plural_label}</MenuItem>
+        })
+      )
+    }
   }
 
+  const handleChangeModule = (event) => {
+    setModuleValue(event.target.value);
+  };
+
+  /**Set table header */
+  function setTableData() {
+    const roleTab = [];
+    roles.forEach(role => {
+      roleTab.push(role.Role);
+    })
+    return roleTab
+  }
+
+
   return (
-    <GridContainer>
-      <Box className={classes.box}>
-        <Button variant="outlined"
-                startIcon={<AddIcon />}  
-                onClick={handleClickOpen}
-          >
-          Créer un role
-        </Button>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Créer un role</DialogTitle>
-          <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="role_name"
-              label="Nom du role"
-              type="text"
-              fullWidth
-              variant="standard"
-              onChange={handleChange}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button fullWidth onClick={()=>createRole()}>Créer</Button>
-          </DialogActions>
-        </Dialog>
-      </Box>
-      <GridItem xs={12} sm={12} md={12}>
-        <Card>
-          <CardHeader color="info">
-            <h4 className={classes.cardTitleWhite}>Simple Table</h4>
-            <p className={classes.cardCategoryWhite}>
-              Here is a subtitle for this table
-            </p>
-          </CardHeader>
-          <CardBody>
-          <Stack direction="row" spacing={2}>
-            <Button href="index.html#/connection">Connexion</Button>
-          </Stack>
-            <Table
-              tableHeaderColor="primary"
-              tableHead={["Name", "Country", "City", "Salary"]}
-              /*tableData={[
-                ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"],
-                ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"],
-                ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"],
-                ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"],
-                ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"],
-                ["Mason Porter", "Chile", "Gloucester", "$78,615"],
-              ]}*/
-            />
-          </CardBody>
-        </Card>
-      </GridItem>
-    </GridContainer>
+    <>
+    {!done ? 
+      (
+        <Box sx={{ display: 'flex' }}>
+          <CircularProgress />
+        </Box>
+      )
+    :
+      (
+        <GridContainer>
+          <Box className={classes.box}>
+            <Button variant="outlined"
+              startIcon={<AddIcon />}  
+              onClick={handleClickOpen}
+            >
+              Créer un role
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Créer un role</DialogTitle>
+              <DialogContent>
+                <FormControl sx={{ m: 1, minWidth: "90%" }} >
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="role_name"
+                    label="Nom du role"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    onChange={handleChange}
+                  />
+                </FormControl>
+                <FormControl sx={{ m: 1, minWidth: "90%" }} >
+                  <InputLabel id="module">Module</InputLabel>
+                  <Select
+                  value={moduleValue}
+                  label="module"
+                  onChange={handleChangeModule}
+                  >
+                  <MenuItem value="">
+                    <em> - Choisir le module - </em>
+                  </MenuItem>
+                  {getModule()}
+                  </Select>
+                </FormControl>
+              </DialogContent>
+              <DialogActions>
+                <Button fullWidth onClick={()=>createRole()}>Créer</Button>
+              </DialogActions>
+            </Dialog>
+          </Box>
+          <GridItem xs={12} sm={12} md={12}>
+            <Card>
+              <CardHeader color="info">
+                <h4 className={classes.cardTitleWhite}>Role & Permission</h4>
+              </CardHeader>
+              <CardBody>
+                <Stack direction="row" spacing={2}>
+                  <Button href="index.html#/connection">Connexion</Button>
+                </Stack>
+                {roles ?
+                  <Table
+                    tableHeaderColor="primary"
+                    tableHead={["Role", "Module"]}
+                    tableData={setTableData()}
+                    tableApi={["Role_name","Module_Name"]}
+                  />
+                  : 
+                  /**Loading */
+                  <Box sx={{ display: 'flex' }}>
+                    <CircularProgress />
+                  </Box>
+                }
+              </CardBody>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      )}
+    </>
   );
 }
