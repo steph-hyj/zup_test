1a03cec78bbd670513d7b07644923ee6691ffaae
Stephane <ungstephane0@gmail.com>
2022-07-06 16:14:02 +0200

    [Update] Create a new function to get more information for quote
    Création d'une fonction qui permet de récupérer les devis par ID afin d'obtenir l'url du devis

--- functions/crm_crud/Controller/Books/QuotesController.js
+++ functions/crm_crud/Controller/Books/QuotesController.js
@@ -17,7 +17,45 @@ exports.getAllQuotes = async(req,res) => {
 			'hostname': HOST,
 			'port': PORT,
 			'method': 'GET',
-			'path': `/api/v3/estimates?organization_id=${req.params.orgID}&customer_id=${req.params.customer_id}`,
+			'path': `/api/v3/estimates?organization_id=${req.params.org_id}&customer_id=${req.params.customer_id}`,
+			'headers': {
+				'Authorization': `Zoho-oauthtoken ${accessToken}`
+			}
+		};
+
+		var data = "";
+		const request = http.request(options, function (response) {
+			response.on('data', function (chunk) {
+				data += chunk;
+			});
+
+			response.on('end', function () {
+				res.setHeader('content-type', 'application/json');
+				res.status(200).send(data)
+			});
+		});
+		request.end();
+    }
+    catch(err)
+    {
+		console.log(err);
+		res.status(500).send({ message: 'Internal Server Error. Please try again after sometime.' })
+    }
+};
+
+/**Get Quote*/
+exports.getQuote = async(req,res) => {
+    try
+    {
+        const catalystApp = catalyst.initialize(req);
+		const userDetails = await tokenController.getUserDetails(catalystApp);
+		const accessToken = await tokenController.getAccessToken(catalystApp, userDetails);
+
+        const options = {
+			'hostname': HOST,
+			'port': PORT,
+			'method': 'GET',
+			'path': `/api/v3/estimates/${req.params.quote_id}?organization_id=${req.params.org_id}`,
 			'headers': {
 				'Authorization': `Zoho-oauthtoken ${accessToken}`
 			}
--- functions/crm_crud/routes/Books/quote.js
+++ functions/crm_crud/routes/Books/quote.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const quoteController = require('../../Controller/Books/QuotesController.js');
 
 /**Get Quotes */
-router.get('/getAllQuotes/:orgID/:customer_id',quoteController.getAllQuotes);
+router.get('/getAllQuotes/:org_id/:customer_id',quoteController.getAllQuotes);
+/**Get quote by quote id */
+router.get('/getQuote/:org_id/:quote_id', quoteController.getQuote);
 
 module.exports = router;
